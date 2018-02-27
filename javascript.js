50a8da87aab50037a99f27688f9f9891ed4e67ea
Unknown <jray414@gmail.com>
2018-02-27 13:27:00 -0800

    Revert "Weather API function"
    This reverts commit 6e1a8da36fc4ff752408ee04302d45b13325b7b9.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,16 @@
 function runAjax(search) {
 
 
-    event.preventDefault();
-    
     var weatherAPIKey = "&appid=c065128b5114e00c480ea5844e8f6cbd";
 
-    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" " + se
+
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + weatherAPIKey;
 
     $.ajax({
         url: weatherURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        $(".city").text("<h1>" + response.name + " Weather Details</h1>");
-        $(".temp").text("Temp (F): " +response.main.temp);
     });
 
     var seatGeekID = "&client_id=MTA2Njg2NTZ8MTUxOTQxODkzNS44Ng"
@@ -37,4 +34,3 @@ $("button").click(function () {
     console.log(search);
     runAjax(search);
 })
-
