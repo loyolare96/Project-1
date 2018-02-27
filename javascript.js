580fcef553de6afddfec8ccbc4970264f0572c02
Dakota Edwards <drakoden2000@gmail.com>
2018-02-27 14:19:22 -0800

    Auto stash before merge of "dakota-edwards" and "origin/loyolabranch"

--- javascript.js
+++ javascript.js
@@ -1,17 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('select').formSelect();
-  });
+})
 
 $(document).ready(function(){
     $('.datepicker').datepicker();
 });
 
 function runAjax(search) {
-
-
     var weatherAPIKey = "&appid=c065128b5114e00c480ea5844e8f6cbd";
-
-
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + weatherAPIKey;
 
     $.ajax({
@@ -22,11 +18,10 @@ function runAjax(search) {
     });
 }
 
+
 function runAjax2(search) {
-    var state = $("#stateSelect :selected").val()
-
-
     var seatGeekID = "&client_id=MTA2Njg2NTZ8MTUxOTQxODkzNS44Ng"
+    var state = $("#stateSelect :selected").val()
 
     var seatGeekURL = "https://api.seatgeek.com/2/events?" + seatGeekID + "&venue.city=" + search + "&venue.state=" + state + "&datetime_utc.gt="
 
@@ -35,25 +30,50 @@ function runAjax2(search) {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-        var title = (response.events[1].short_title)
-        var ticketUrl = (response.events[1].url)
-        var address = (response.events[1].venue.address)
-        console.log(title)
-        console.log(ticketUrl)
-        console.log(address)
+        var results = response.events;
+        for (i = 0; i < results.length; i++) {
+            var seatGeekDiv = $("#seatGeekInfo");
+            console.log(results[i].title);
+            console.log(results[i].venue.address);
+            console.log(results[i].url);
+            imgTag = $("<img>");
+            newRow = $("<div class='row'>");
+            searchUrl = results[i].url;
+            if (results[i].performers["0"].images.huge != null) {
+                imgTag.attr("src", results[i].performers["0"].images.huge);
+            }
+            else {
+                imgTag.attr("src", "assets/images/null.png");
+            }
+            imgTag.attr("id", "imagelol")
+            newRow.append(imgTag);
+            newRow.append("<br>");
+            newRow.append("Title: " + results[i].title + "<br>");
+            newRow.append("Time: " + results[i].datetime_local + "<br>")
+            if (results[i].venue.address != null) {
+                newRow.append(" Address: " + results[i].venue.address + "<br>" + " Venue: " + results[i].venue.name + "<br>");
+            }
+            if (results[i].url != null) {
+                newRow.append(" URL: " + searchUrl);
+            }
+            if (searchUrl != null) {
+                var urlString = String(searchUrl)
+                newRow.append("<br>");
+                newRow.append('<a href="' + urlString + '"target="_blank">More Info</a>');
+
+            }
+            seatGeekDiv.append(newRow);
+            seatGeekDiv.append("<br>");
+
+        }
     })
-
 }
-
-
 var search = ''
 
-$(".btn").click(function () {
-    search = $("#searchBar").val(),
-    
+$("#searchBtn").click(function () {
+    $("#seatGeekInfo").empty();
+    search = $("#searchBar").val();
+    console.log(search);
     runAjax(search);
     runAjax2(search);
-    
-
-
 })
